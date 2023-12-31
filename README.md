1fab50d07031febda827725001fc0ebf3e85bf74
Brock Shamblin <shamblin.brock@gmail.com>
2023-12-31 03:17:36 +0000

    Clean up + README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # PlaylistConverter
+
+This tool converts an Apple Music playlist to a Spotify playlist. It needs a link to the Apple Music playlist and authorization to a Spotify account.
+
+## Prerequisits
+
+You will need Python > 3.6 and all the modules used in `convert.py`
+
+You will need to create an app on the Spotify developer dashboard. See the [Spotify](https://developer.spotify.com/documentation/web-api/tutorials/getting-started) page to get started with creating the app and gathering credentials. For the `redirect URI`, use something like `http://localhost:3000`, this server will start automatically for you.
+
+You will use the credentials from the Spotify app you just created to fill out the `.env` file. You will copy the client ID, client secret, and redirect URI to the corresponding fields in `.env`
+
+## Running
+
+`py convert.py [-h] -p PLAYLIST [-d DESCRIPTION] [-u URL]`
+
+The only required argument is `-p PLAYLIST`, where `PLAYLIST` is the name of the playlist you want created on Spotify. When you run this for the first time, a browser window will popup to ask for permission to your Spotify account. Accept this and the window will automatically close. 
+
+If the `URL` argument is left blank, the program will ask for this value to be input in the terminal. This is the URL to the Apple Music playlist you want to be copied to Spotify (e.g. `https://music.apple.com/us/playlist/<PLAYLIST_NAME>/<UNIQUE_ID>`)
--- convert.py
+++ convert.py
@@ -20,6 +20,8 @@ def get_args():
                         help='Name of Playlist')
     parser.add_argument('-d', '--description', required=False, default='',
                         help='Description of Playlist')
+    parser.add_argument('-u', '--url', required=False, default='',
+                        help='Apple Music URL')
     return parser.parse_args()
 
 # Return the URI of the first song returned when searching the song title and artist name
@@ -28,20 +30,26 @@ def find_song(song, artist):
 
 def main():
     args = get_args()
+
+    apple_url = args.url
+    if apple_url == '':
+        apple_url = input('Enter the URL of the Apple Music Playlist: ')
+
+    # Create a new playlist with the given name from command line args
     user = sp.me()['id']
     pl = sp.user_playlist_create(user, args.playlist, description=args.description)
 
-    # apple_url = 'https://music.apple.com/us/playlist/me-bitch/pl.u-NpXm6X3FmM0lNAV'
-    apple_url = input('Enter the URL of the Apple Music Playlist: ')
-
+    # Don't want to spend $100 to access the apple music API, so parse the raw HTML instead
     soup = BeautifulSoup(requests.get(url=apple_url).content, 'html.parser')
 
     found = soup.find(id="serialized-server-data")
 
+    # Dig out the JSON from the HTML page
     df = json.loads(str(found.string)[1:-1]) # Remove leading/trailing brackets
     data = pd.json_normalize(df['data'], max_level=10)
     song_titles = data['sections'][0][1]['items']
 
+    # Loop through each song from the apple music page
     songs = []
     for song in song_titles:
         title = song['title']
@@ -49,15 +57,16 @@ def main():
 
         print(f'{title} {artist}')
 
+        # Search for the song on Spotify to get the URI 
         songs.append(find_song(title, artist))
 
+        # Spotify has a 100 song limit per request, so just upload periodically
         if len(songs) >= 50:
-            # Spotify has a 100 song limit per request, so just upload periodically
             sp.playlist_add_items(pl['id'], songs)
             songs = []
 
+    # Add the remaining songs to the Spotify playlist
     sp.playlist_add_items(pl['id'], songs)
-
 
 if __name__ == '__main__':
     main()
